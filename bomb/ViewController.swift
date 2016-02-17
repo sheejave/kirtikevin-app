c6481f46cc2372b23f7a20697aeecb48332517c3
Manjit Rajaretnam <manjitrajaretnam@manjits-mbp.home>
2016-02-17 13:20:44 -0600

    Initial Commit

---
+++ bomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bomb
+//
+//  Created by Manjit Rajaretnam on 2/17/16.
+//  Copyright © 2016 Kevin Awesomeness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
