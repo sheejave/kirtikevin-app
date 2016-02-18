8551f618b48a314ec9b6ae72efa4324a22578a05
Manjit Rajaretnam <manjitrajaretnam@Manjits-MBP.home>
2016-02-18 13:11:14 -0600

    Added files for kirtikevin

--- bomb/ViewController.swift
+++ bomb/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var purpleBomb: UIImageView!
+    
+    @IBOutlet weak var greenBomb: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,16 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func hidePurpleBomb(sender: AnyObject) {
+        purpleBomb.hidden = true
+        
+        greenBomb.hidden = false
+    }
+    
+    @IBAction func hideGreenBomb(sender: AnyObject) {
+        greenBomb.hidden = true
+        
+        purpleBomb.hidden = false
+    }
 }
 
